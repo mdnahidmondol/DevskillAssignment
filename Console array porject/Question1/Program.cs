3d69e77be45dc0cf558ad938dc52935a4cf6df1f
Md Nahid Mondol <nahidmondol444@gmail.com>
2021-05-02 14:55:26 +0000

    One error have

--- Console array porject/Question1/Program.cs
+++ Console array porject/Question1/Program.cs
@@ -6,7 +6,27 @@ namespace Question1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][,] jaggedArray4 = new int[][,]
+            {
+                new int [,] { { 1, 3}, { 5 , 7} },
+                new int [,] { { 0, 2}, { 4, 6}, { 8, 10} },
+                new int [,] { { 11,22}, { 99, 88}, { 0, 9} }
+            };
+
+            for (int i = 0; i < jaggedArray4.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray4[i].GetLength(0); j++)
+                {
+                    for (int k = 0; k < jaggedArray4[j].Rank; k++)
+                    {
+                        Console.Write($"JaggedArray[{i}] [{j}, {k}]: {jaggedArray4[i][j, k]} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
         }
+        
     }
 }
